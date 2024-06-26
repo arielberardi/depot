488f773b0e38b2c0e7021c1fea84683ddd3a4146
Ariel Berardi <ariel.berardi95@gmail.com>
2024-06-26 17:56:51 +0100

    Add test for stores

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -4,8 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Stores' do
   describe 'GET /index' do
-    before { get store_index_path }
+    before do
+      create_list(:product, 2)
+      get store_index_path
+    end
 
     it { expect(response).to have_http_status(:success) }
+
+    describe 'shows the list of products' do
+      it { expect(response.body).to include(Product.first.title) }
+      it { expect(response.body).to include(Product.last.title) }
+    end
   end
 end
