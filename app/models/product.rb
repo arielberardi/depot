6e5fcc65d79aae7b4f6e8a379a52785b031f507e
Ariel Berardi <ariel.berardi95@gmail.com>
2024-06-26 09:35:58 +0100

    Fix rubocop issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   validates :title, :image_url, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|jpeg|png)\z}i,
+    with: /\.(gif|jpg|jpeg|png)\z/i,
     message: 'must be an URL for GIF, JPEG, or PNG image.'
   }
 end
