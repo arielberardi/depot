aff2d26ba7bb73cda02f8605b13aca9900399219
Ariel Berardi <ariel.berardi95@gmail.com>
2024-06-28 08:04:58 +0100

    Add qquantity for line items under same product id

--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -4,19 +4,33 @@ require 'rails_helper'
 
 RSpec.describe '/line_items' do
   describe 'POST /create' do
-    context 'with valid parameters' do
-      let!(:product) { create(:product) }
+    let!(:product) { create(:product) }
 
-      it 'creates a new LineItem' do
+    it 'creates a new LineItem' do
+      expect do
+        post line_items_url, params: { product_id: product.id }
+      end.to change(LineItem, :count).by(1)
+    end
+
+    context 'when product is already in the line items' do
+      before { post line_items_url, params: { product_id: product.id } }
+
+      it 'does not create a new line item' do
         expect do
           post line_items_url, params: { product_id: product.id }
-        end.to change(LineItem, :count).by(1)
+        end.not_to change(LineItem, :count)
       end
 
-      it 'redirects to the created line_item' do
-        post line_items_url, params: { product_id: product.id }
-        expect(response).to redirect_to(cart_url(LineItem.last.cart))
+      it 'updates the quantity of line item' do
+        expect do
+          post line_items_url, params: { product_id: product.id }
+        end.to change { LineItem.last.quantity }.by(1)
       end
+    end
+
+    it 'redirects to the created line_item' do
+      post line_items_url, params: { product_id: product.id }
+      expect(response).to redirect_to(cart_url(LineItem.last.cart))
     end
 
     context 'with invalid parameters' do
