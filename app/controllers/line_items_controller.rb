41bb6124579c2a43a5f5f2429f07627bb16a9f62
Ariel Berardi <ariel.berardi95@gmail.com>
2024-06-29 09:57:52 +0100

    Add feature to show total price price in carts view

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -11,7 +11,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product)
 
     if @line_item.save
-      redirect_to cart_url(@line_item.cart), notice: I18n.t('line_item.notice.created')
+      redirect_to cart_url(@line_item.cart)
     else
       render :new, status: :unprocessable_entity
     end
