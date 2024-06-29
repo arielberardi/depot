d96caf2bd482a140fac779b6fc5459c9c8d74dfe
Ariel Berardi <ariel.berardi95@gmail.com>
2024-06-29 15:44:01 +0100

    Make updates in products to be turbo stream

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,7 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1 or /products/1.json
   def update
     if @product.update(product_params)
+      @product.broadcast_replace_later_to 'products', partial: 'store/product'
       redirect_to product_url(@product), notice: I18n.t('product.notice.updated')
     else
       render :edit, status: :unprocessable_entity
