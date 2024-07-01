877f5e2afbe6e3b62c6e4d9581650ae1ba054908
Ariel Berardi <ariel.berardi95@gmail.com>
2024-07-01 08:13:57 +0100

    Add  locale and translations to spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale_from_params
+
+  protected
+
+  def set_locale_from_params
+    return unless params[:locale]
+
+    if I18n.available_locales.map(&:to_s).include?(params[:locale])
+      I18n.locale = params[:locale]
+    else
+      flash.now[:notice] = "Missing translations for #{params[:locale]}"
+      logger.error = "Missing translations for #{params[:locale]}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
-  resources :line_items, only: %i[create]
-  resources :carts, only: %i[show destroy]
-  resources :products
-  resources :orders
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root 'store#index', as: 'store_index'
+  resources :users
+
+  scope '(:locale)' do
+    resources :products
+    resources :line_items, only: %i[create]
+    resources :carts, only: %i[show destroy]
+    resources :orders
+    root 'store#index', as: 'store_index'
+  end
 end
